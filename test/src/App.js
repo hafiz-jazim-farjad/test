3d29dd314f2537060f7686f842b4e4147b6ffca9
Hafiz Jazim Farjad <jazim52946@gmail.com>
2024-08-27 15:11:26 +0500

    update

--- test/src/App.js
+++ test/src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <>
-      <div class="navBar"> </div>
+      <div class='navBar'>
+        <div class='.logo'> Logo </div>
+        <div class='.menu'>
+          <ul>
+            <li> Home </li>
+            <li> About </li>
+            <li> Contact </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
